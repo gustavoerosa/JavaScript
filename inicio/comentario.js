659afc5f521e127042de605a04937d05ef1f5f93
Guss <gustavoe.rosa@hotmail.com>
2021-12-21 09:08:12 -0300

    Udemy - Curso Web Moderno Completo 2021 - Seção 3

---
+++ inicio/comentario.js
@@ -0,0 +1,9 @@
+// console.log é utilizado para exibir algo na tela do computador!
+console.log("Passo #01");
+console.log("Passo #02"); // mais um exemplo de comentario!
+console.log("Passo #03");
+
+/*
+    Esse é um comentario
+    de multiplas linhas!
+*/
