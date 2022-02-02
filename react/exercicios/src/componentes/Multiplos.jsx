744a45d15041156c566ddc602e44e36eed3e78f0
Guss <gustavoe.rosa@hotmail.com>
2022-02-02 19:39:04 -0300

    Udemy - Curso Web Moderno Completo 2021 - Seção 22

---
+++ react/exercicios/src/componentes/Multiplos.jsx
@@ -0,0 +1,6 @@
+export const BoaTarde = props => <h1>Boa tarde {props.nome}!</h1>
+
+const BoaNoite = props => <h1>Boa noite {props.nome}!</h1>
+
+export { BoaNoite }
+export default { BoaTarde, BoaNoite }
